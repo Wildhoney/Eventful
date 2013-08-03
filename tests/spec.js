b2358bc23827b0bcd41f5a5ae11845c80cbc34da
Adam Timberlake <adam.timberlake@gmail.com>
2013-08-03 23:30:44 +0100

    Updated example to take into account prototype version. Relates to #1

--- tests/spec.js
+++ tests/spec.js
@@ -10,7 +10,7 @@ describe('Eventful', function() {
             }
         };
 
-        //eventful.applyPrototypes();
+        eventful.applyPrototypes();
         eventful.clearSubscriptions();
 
     });
@@ -18,30 +18,61 @@ describe('Eventful', function() {
     describe('Subscriptions', function() {
 
         it('Can subscribe to a non-existent token.', function () {
-           eventful.subscribe(myObject, 'getName', 'name');
-           expect(eventful.getSubscriptions().length).toEqual(1);
-           expect(eventful.getSubscriptions()[0].token).toEqual('name');
+
+            // Non-prototype.
+            eventful.subscribe(myObject, 'getName', 'name');
+            expect(eventful.getSubscriptions().length).toEqual(1);
+            expect(eventful.getSubscriptions()[0].token).toEqual('name');
+
+            // Prototype.
+            myObject.getName.subscribe('name');
+            expect(myObject.getSubscriptions().length).toEqual(1);
+            expect(myObject.getSubscriptions()[0].token).toEqual('name');
+
         });
 
         it('Can subscribe to an existent token.', function() {
-           eventful.subscribe(myObject, 'getName', 'name');
-           eventful.subscribe(myObject, 'getName', 'name');
-           expect(eventful.getSubscriptions().length).toEqual(1);
-           expect(eventful.getSubscriptions()[0].token).toEqual('name');
+
+            // Non-prototype.
+            eventful.subscribe(myObject, 'getName', 'name');
+            eventful.subscribe(myObject, 'getName', 'name');
+            expect(eventful.getSubscriptions().length).toEqual(1);
+            expect(eventful.getSubscriptions()[0].token).toEqual('name');
+
+            // Prototype.
+            myObject.getName.subscribe('name');
+            myObject.getName.subscribe('name');
+            expect(myObject.getSubscriptions().length).toEqual(1);
+            expect(myObject.getSubscriptions()[0].token).toEqual('name');
+
         });
 
         it('Can unsubscribe individually from existing tokens.', function() {
+
+            // Non-prototype.
             eventful.subscribe(myObject, 'getName', 'name');
             eventful.unsubscribe(myObject, 'getName', 'name');
             expect(eventful.getSubscriptions().length).toEqual(0);
+
+            // Prototype.
+            myObject.getName.subscribe('name');
+            myObject.getName.unsubscribe('name');
+            expect(eventful.getSubscriptions().length).toEqual(0);
+
         });
 
         it('Can unsubscribe globally from existing tokens.', function() {
+
+            // Non-prototype.
             eventful.subscribe(myObject, 'getName', 'name');
-            eventful.subscribe(myObject, 'getName', 'name');
-            eventful.subscribe(myObject, 'getName', 'name');
-            eventful.unsubscribe(myObject, 'name');
+            eventful.unsubscribe(myObject, 'getName', 'name');
             expect(eventful.getSubscriptions().length).toEqual(0);
+
+            // Prototype.
+            myObject.getName.subscribe('name');
+            eventful.unsubscribe('name');
+            expect(eventful.getSubscriptions().length).toEqual(0);
+
         });
 
     });
